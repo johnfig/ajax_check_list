326adca226619f11c30048fc7c37a46a5ba46905
John Figueiredo <john@johnfigueiredo.com>
2015-01-30 10:35:13 -0800

    Reset to railscast before application to not build all of the app out from scratch

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,12 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key is used for verifying the integrity of signed cookies.
+# Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
-
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-AjaxCheckList::Application.config.secret_key_base = '434f8c47208976d1fd74375c9c16ba214082690d4c088b8c12d1a7d278f3de4b9844e5d494e6929d19bcce51d0db0e602834b3af965c1c07c5c30e8f97a06cdf'
+Checklist::Application.config.secret_token = '26f06802dd24025a5963321865a04c0cb17a14618c1d6349ee4fc41cd52c24b4bb972986d8885d1f7852d7f233fda4bcd743f60a2170717c877223c3d0fdd828'
