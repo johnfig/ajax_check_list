5a8907faddeb6ac4ec27f552864fe4a705064313
John Figueiredo <john@johnfigueiredo.com>
2015-02-02 12:54:24 -0800

    Add update and destroy actions to ajax app

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -20,11 +20,17 @@ class TasksController < ApplicationController
   def update
     @task = Task.find(params[:id])
     @task.update_attributes!(params[:task])
-    redirect_to tasks_url
+    respond_to do |format|
+      format.html { redirect_to tasks_url }
+      format.js
+    end
   end
 
   def destroy
     @task = Task.destroy(params[:id])
-    redirect_to tasks_url
+    respond_to do |format|
+      format.html { redirect_to tasks_url }
+      format.js
+    end
   end
 end
